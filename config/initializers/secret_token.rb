7874265d3d3b98fe261306d97f41cf1a31d592ec
AJ <ajhoward14@gmail.com>
2014-03-21 20:58:28 -0700

    start me up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Septa::Application.config.secret_token = '8955a853c7a88ab226c69553ff189534e6408b6057f15ef6837c73ec417eab11700db3c93c4c2a793a71f2beb838a9324700291e2f2821b973a7cebc1ea4afd0'
